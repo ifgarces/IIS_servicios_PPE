53e41643f4af22273da497cee78e98e68fa55afb
DiegoChristie <43647355+DiegoChristie@users.noreply.github.com>
2021-10-18 22:49:52 -0300

    1st Iteration Database Table

--- database/tables.sql
+++ database/tables.sql
@@ -1,1 +1,21 @@
-/* TODO */
+----------------------------------------------------------------------------------------------------
+-- For creating database tables.
+----------------------------------------------------------------------------------------------------
+
+-- Commanding Postgres to stop file execution on error
+\set ON_ERROR_STOP true
+
+DROP TABLE IF EXISTS TransaccionTGR;
+
+CREATE TABLE TransaccionTGR(
+    folio                  serial,
+    id_persona             varchar(10) NOT NULL, -- 12345678-K
+    numero_repertorio      varchar(12) NOT NULL,   
+    timestamp_recepcion    datetime2,
+    monto                  decimal NOT NULL,
+    estado_transaccion     varchar(10),
+    fecha_aprobacion       datetime2
+    ingreso                boolean,
+    estado_TGR             varchar(15),
+	    PRIMARY KEY (folio)
+);
